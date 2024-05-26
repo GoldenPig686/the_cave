2c8efbb9b03c204ab9c1a292b277abd725363a81
天才绣工 <aaronblack45@outlook.com>
2024-05-26 21:36:27 +0800

    add the feature of cursor

--- scripts/alia.gd
+++ scripts/alia.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 const SPEED = 130
 const JUMP_VELOCITY = -220
 
+var sprite_direction: int = 1
+
 var direction: float = 0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -37,9 +39,24 @@ func _process(_delta: float) -> void:
 	if direction == 0:
 		animated_sprite_2d.play("idle")
 	else:
-		animated_sprite_2d.play("run")
-		animated_sprite_2d.flip_h = direction < 0
+		var is_play_backwards: bool = direction * sprite_direction < 0
+		if is_play_backwards:
+			animated_sprite_2d.play_backwards("run")
+		else:
+			animated_sprite_2d.play("run")
 	
 	# Set vertical animation.
 	if not is_on_floor():
 		animated_sprite_2d.play("jump")
+	
+	# Set the animation according to the global mouse position. That is, make
+	# the animation always faces the cursor.
+	if (
+		global_position.direction_to(
+			get_global_mouse_position()
+		).dot(
+			Vector2(sprite_direction, 0)
+		) < 0
+	):
+		animated_sprite_2d.flip_h = !animated_sprite_2d.flip_h
+		sprite_direction = -sprite_direction
